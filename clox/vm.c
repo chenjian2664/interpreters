f65f943c75996374abc3e751d35283311ffede02
chenjian2664 <chenjian2664@gmail.com>
2023-10-13 18:03:56 +0800

    Add negate

--- clox/vm.c
+++ clox/vm.c
@@ -6,7 +6,11 @@
 
 VM vm;
 
+static InterpretResult run();
+static void resetStack();
+
 void initVM() {
+    resetStack();
 }
 
 void freeVM() {
@@ -25,22 +29,50 @@ static InterpretResult run() {
     for (;;) {
 
 #ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
         disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
-        case OP_CONSTANT: {
-            Value constant = READ_CONSTANT();
-            printValue(constant);
-            printf("\n");
-            break;
-        }
-        case OP_RETURN: {
-            return INTERPRET_OK;
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                push(constant);
+//              printValue(constant);
+//              printf("\n");
+                break;
+            }
+            case OP_RETURN: {
+                printValue(pop());
+                printf("\n");
+                return INTERPRET_OK;
+            }
+            case OP_NEGATE: {
+                push(-pop());
+                break;
+            }
         }
     }
 }
 
+static void resetStack() {
+    vm.stackTop = vm.stack;
+}
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
 #undef READ_BYTE
 #undef READ_CONSTANT
-}
