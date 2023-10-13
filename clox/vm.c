1e6cc404fa6c5be38410f58f7903451e4c6b0c76
chenjian2664 <chenjian2664@gmail.com>
2023-10-13 18:13:01 +0800

    Add binary

--- clox/vm.c
+++ clox/vm.c
@@ -25,6 +25,12 @@ InterpretResult interpret(Chunk* chunk) {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
 
     for (;;) {
 
@@ -52,10 +58,11 @@ static InterpretResult run() {
                 printf("\n");
                 return INTERPRET_OK;
             }
-            case OP_NEGATE: {
-                push(-pop());
-                break;
-            }
+            case OP_ADD:      BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE:   BINARY_OP(/); break;
+            case OP_NEGATE:   push(-pop()); break;
         }
     }
 }
@@ -76,3 +83,5 @@ Value pop() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
+
